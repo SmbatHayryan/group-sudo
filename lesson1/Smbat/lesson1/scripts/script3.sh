1d7d9727dbb59ef0b1e959ea6a3301395df8e896
SmbatHayryan <smbathayryan@gmail.com>
2018-03-06 23:39:45 +0400

    modified scripts

--- lesson1/Smbat/lesson1/scripts/script3.sh
+++ lesson1/Smbat/lesson1/scripts/script3.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-cd /workspace/lessons/lesson1/output/u1/u2 && mv a.txt b.txt /workspace/lessons/lesson1/output/u1/u3
-cd /workspace/lessons/lesson1/output/u1 && rm a.txt 
+cd ../output/u1/u2
+mv a.txt ../u3/a.txt 
+mv b.txt ../u3/b.txt
+cd ../ && rm a.txt
